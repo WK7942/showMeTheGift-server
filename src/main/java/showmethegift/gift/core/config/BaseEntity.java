42d2126444641482d13f229c805b0b38ac81e2e9
josh <josh@myfair.co>
2022-03-18 13:34:05 +0900

    feat: Base Entity 생성

--- src/main/java/showmethegift/gift/core/config/BaseEntity.java
+++ src/main/java/showmethegift/gift/core/config/BaseEntity.java
@@ -1,14 +1,28 @@
 package showmethegift.gift.core.config;
 
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long num;
 
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    public void setNum(Long num) {
+        this.num = num;
+    }
 }
